07617ec36c74bdab12a007ed9eaa4621031cd744
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2012-10-07 19:49:09 -0400

    Refactoring xml generation

--- healthvault/healthvault.py
+++ healthvault/healthvault.py
@@ -2,12 +2,13 @@
 #
 # Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
 #
+# note: method names match the HV API but with a leading lowercase letter
+#
 # Todo
 # - refactor using lxml's CSSSelectors (see 'csss') and E-factory?
 #   <http://lxml.de/tutorial.html#the-e-factory>
 # - add newlines and spacing to the templates
 # - do a regex compacting multiple spaces and newlines
-
 import base64
 from   Crypto.Signature import PKCS1_v1_5
 from   Crypto.Hash import SHA
@@ -44,6 +45,8 @@ class HVConn(object):
     _shared_secret = None
     _private_key = None
     _version = '2.0.0.0'
+    _language = 'en'
+    _country = 'US'
     _ttl = '1800'
 
     person = HVPerson()
@@ -72,7 +75,6 @@ class HVConn(object):
         signer = PKCS1_v1_5.new(self._private_key)
         return base64.encodestring(signer.sign(SHA.new(data)))
 
-
     def _send_request(self, payload):
         conn = httplib.HTTPSConnection(settings.HV_SERVICE_SERVER, 443)
         conn.putrequest('POST', '/platform/wildcat.ashx')
@@ -90,6 +92,33 @@ class HVConn(object):
             raise
         else:
             return resp
+
+    def _send_request_and_get_dom(self, payload):
+        conn = httplib.HTTPSConnection(settings.HV_SERVICE_SERVER, 443)
+        conn.putrequest('POST', '/platform/wildcat.ashx')
+        conn.putheader('Content-Type', 'text/xml')
+        conn.putheader('Content-Length', '%d' % len(payload))
+        conn.endheaders()
+        try:
+            conn.send(payload)
+        except socket.error, v:
+            if v[0] == 32:      # Broken pipe
+                conn.close()
+            raise
+        resp = conn.getresponse()
+        if resp.status != 200:
+            raise
+        else:
+            dom = minidom.parseString(resp.read())
+            if DEBUG:
+                print self._pretty_print_dom(dom)
+
+            el = dom.getElementsByTagName('code')[0]
+            code = self._get_single_el_value(dom, 'code')
+            if code != '0':
+                raise 'Non-zero return code in _send_request_and_get_dom()'
+            else:
+                return dom
 
     def _authenticate(self):
         content_tmpl = string.Template("""
@@ -143,12 +172,7 @@ class HVConn(object):
 
         }).translate(None, '\n')
 
-        response = self._send_request(payload)
-        auth_response = response.read()
-        dom = minidom.parseString(auth_response)
-        #if DEBUG:
-            #self._pretty_print_dom(dom)
-        # stop the unicode infection with str()!
+        dom = self._send_request_and_get_dom(payload)
         self._auth_token = str(self._get_single_el_value(dom, 'token'))
         self._shared_secret = str(self._get_single_el_value(dom, 'shared-secret'))
 
@@ -160,109 +184,70 @@ class HVConn(object):
             self._user_auth_token = str(user_auth_token)
             self.getPersonInfo()
 
-    # note: method names match the HV API but with a leading lowercase letter
-    def getPersonInfo(self):
-        # create the complete <header>
-        header_tmpl = string.Template("""<header><method>GetPersonInfo</method><method-version>1</method-version><auth-session><auth-token>$AUTH_TOKEN</auth-token><user-auth-token>$USER_AUTH_TOKEN</user-auth-token></auth-session><language>en</language><country>US</country><msg-time>$NOW</msg-time><msg-ttl>$TTL</msg-ttl><version>$VERSION</version><info-hash><hash-data algName="SHA256">$HASH_DATA</hash-data></info-hash></header>""")
-
-        info_str = '<info></info>'
-        hash_data_str = base64.b64encode(hashlib.sha256(info_str).digest()).strip()
-
-        header_str = header_tmpl.substitute({
+    def _build_header(self, method, method_version, hash_data, record_id=None):
+        """ Create the header string. record_id is optional """
+        header = '<header><method>$METHOD</method><method-version>$METHOD_VERSION</method-version>'
+        if record_id:
+            header = header + '<record-id>'+record_id+'</record-id>'
+        header = header + '<auth-session><auth-token>$AUTH_TOKEN</auth-token><user-auth-token>$USER_AUTH_TOKEN</user-auth-token></auth-session><language>$LANGUAGE</language><country>$COUNTRY</country><msg-time>$NOW</msg-time><msg-ttl>$TTL</msg-ttl><version>$VERSION</version><info-hash><hash-data algName="SHA256">$HASH_DATA</hash-data></info-hash></header>'
+        return string.Template(header).substitute({
+            'METHOD': method,
+            'METHOD_VERSION': method_version,
             'AUTH_TOKEN': self._auth_token,
             'USER_AUTH_TOKEN': self._user_auth_token,
+            'LANGUAGE': self._language,
+            'COUNTRY': self._country,
             'NOW': self._now_in_iso(),
             'TTL': self._ttl,
             'VERSION': self._version,
-            'HASH_DATA': hash_data_str
+            'HASH_DATA': hash_data,
+            'RECORD_ID': record_id
         })
 
-        # hmac the complete <header> (don't forget to b64 decode the secret)
+    def _build_header_hmac(self, header):
         h = hmac.new(base64.b64decode(self._shared_secret),
-                header_str,
-                hashlib.sha256)
-        hmac_data_str = base64.b64encode(h.digest())
+                     header,
+                     hashlib.sha256)
+        return base64.b64encode(h.digest())
 
-        # build the final <request>
+    def _build_request(self, header, info):
+        # NOTE: don't add spaces and newlines here!
         req_tmpl = string.Template("""<wc-request:request xmlns:wc-request="urn:com.microsoft.wc.request"><auth><hmac-data algName="HMACSHA256">$HMAC</hmac-data></auth>$HEADER$INFO</wc-request:request>""")
-        req_str = req_tmpl.substitute({
-            'HMAC': hmac_data_str,
-            'HEADER': header_str,
-            'INFO': info_str
+        return req_tmpl.substitute({
+            'HMAC': self._build_header_hmac(header),
+            'HEADER': header,
+            'INFO': info
         })
 
-        #req_str = re.sub(' {2,}', '', req_str.strip())
-        #print '---\n'+req_str+'\n---'
+    def _create_request(self, info, method, method_version='1', record_id=None):
+        hash_data = base64.b64encode(hashlib.sha256(info).digest()).strip()
+        header = self._build_header(method=method,
+                                    method_version=method_version,
+                                    hash_data=hash_data,
+                                    record_id=record_id)
+        return self._build_request(header, info)
 
-        response = self._send_request(req_str)
-        dom = minidom.parseString(response.read())
-        if DEBUG:
-            print self._pretty_print_dom(dom)
-
-        el = dom.getElementsByTagName('code')[0]
-        code = str(el.firstChild.nodeValue)
-        code = self._get_single_el_value(dom, 'code')
-        if code != '0':
-            raise 'Non-zero return code in getPersonInfo'
-        else:
-            self.person.person_id = self._get_single_el_value(dom, 'person-id')
-            self.person.name = self._get_single_el_value(dom, 'name')
-            # todo: extract more record data here
-            self.person.selected_record_id = self._get_single_el_value(dom, 'selected-record-id')
-            self._record_id = self.person.selected_record_id
-        #if DEBUG:
-            #print person.person_id
-            #print person.name
+    def getPersonInfo(self):
+        # TODO: extract more record data
+        # <record app-record-auth-action="NoActionRequired" app-specific-record-id="218697" auth-expires="9999-12-31T23:59:59.999Z" date-created="2012-09-19T16:07:52.507Z" date-updated="2012-09-24T19:22:00.877Z" display-name="Arjun" id="f9982b79-4369-4357-8268-0b344941ab02" location-country="US" max-size-bytes="4294967296" record-custodian="true" rel-name="Self" rel-type="1" size-bytes="3167" state="Active">
+        dom = self._send_request_and_get_dom(
+            self._create_request('<info></info>', 'GetPersonInfo')
+        )
+        self.person.person_id = self._get_single_el_value(dom, 'person-id')
+        self.person.name = self._get_single_el_value(dom, 'name')
+        self.person.selected_record_id = self._get_single_el_value(
+                                            dom,
+                                            'selected-record-id')
+        self._record_id = self.person.selected_record_id
 
     def getThings(self, type):
-        # todo: hardcoding for now... refactor with above
-        # need to supply some context here: either record, person or app id
-        # potentially same has header_tmpl above then
-        header_tmpl = string.Template("""<header><method>GetThings</method><method-version>1</method-version><record-id>$RECORD_ID</record-id><auth-session><auth-token>$AUTH_TOKEN</auth-token><user-auth-token>$USER_AUTH_TOKEN</user-auth-token></auth-session><language>en</language><country>US</country><msg-time>$NOW</msg-time><msg-ttl>$TTL</msg-ttl><version>$VERSION</version><info-hash><hash-data algName="SHA256">$HASH_DATA</hash-data></info-hash></header>""")
-
-        # this is the query
-        info_str = \
-        '<info><group><filter><type-id>'+type+'</type-id></filter><format></format></group></info>'
-        hash_data_str = base64.b64encode(hashlib.sha256(info_str).digest()).strip()
-
-        header_str = header_tmpl.substitute({
-            'RECORD_ID': self._record_id,
-            'AUTH_TOKEN': self._auth_token,
-            'USER_AUTH_TOKEN': self._user_auth_token,
-            'NOW': self._now_in_iso(),
-            'TTL': self._ttl,
-            'VERSION': self._version,
-            'HASH_DATA': hash_data_str
-        })
-
-        # hmac the complete <header> (don't forget to b64 decode the secret)
-        h = hmac.new(base64.b64decode(self._shared_secret),
-                header_str,
-                hashlib.sha256)
-        hmac_data_str = base64.b64encode(h.digest())
-
-        # build the final <request>
-        req_tmpl = string.Template("""<wc-request:request xmlns:wc-request="urn:com.microsoft.wc.request"><auth><hmac-data algName="HMACSHA256">$HMAC</hmac-data></auth>$HEADER$INFO</wc-request:request>""")
-        req_str = req_tmpl.substitute({
-            'HMAC': hmac_data_str,
-            'HEADER': header_str,
-            'INFO': info_str
-        })
-
-        #req_str = re.sub(' {2,}', '', req_str.strip())
-        #print '---\n'+req_str+'\n---'
-        response = self._send_request(req_str)
-        dom = minidom.parseString(response.read())
-        if DEBUG:
-            print self._pretty_print_dom(dom)
-
-        el = dom.getElementsByTagName('code')[0]
-        code = str(el.firstChild.nodeValue)
-        code = self._get_single_el_value(dom, 'code')
-        if code != '0':
-            raise # 'Non-zero return code in getPersonInfo'
-        else:
-            return dom
+        info = \
+            '<info><group><filter><type-id>'+type+'</type-id></filter><format></format></group></info>'
+        return self._send_request_and_get_dom(
+            self._create_request(info,
+                                 'GetThings',
+                                 record_id=self._record_id)
+        )
 
     def getThingById(self, id):
         # todo: hardcoding for now... refactor with above
